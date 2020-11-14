c1f2aa480e47bbec324fef56ad86ad93f5ffd09b
Velovatik <velovatiykirill@gmail.com>
2020-11-14 17:38:14 +0300

    Added - function

---
+++ Commit_3.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int sum (int a, int b) // функция вычисления суммы 
+{
+    int c = a + b;  
+	return c;  
+}
+
+int mult (int a, int b) // функция вычисления умножения 
+{
+    int c = a * b; 
+	return c;  е
+}
+
+int dif (int a, int b) // функция вычисления вычитания 
+{
+    int c = a - b;  
+	return c;  
+}

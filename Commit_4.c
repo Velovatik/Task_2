13fd4b5bfd25bea9e9a99f0a3cb717ae2633bcdf
Velovatik <velovatiykirill@gmail.com>
2020-11-14 17:44:34 +0300

    Added divide function

---
+++ Commit_4.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int sum (int a, int b) // функция вычисления суммы 
+{
+    int c = a + b;  
+	return c;  
+}
+
+int mult (int a, int b) // функция вычисления умножения 
+{
+    int c = a * b; 
+	return c;  е
+}
+
+int dif (int a, int b) // функция вычисления вычитания 
+{
+    int c = a - b;  
+	return c;  
+}
+
+int div (int a, int b) // функция вычисления деления 
+{
+    int c = a / b;  
+	return c;  
+}
